ee88f577541f711cc12972acd193b2aa5f81be6f
malymary <malyutina.mary@gmail.com>
2023-04-28 15:22:59 +0800

    modified napoleon BFS

--- napoleon_BFS.py
+++ napoleon_BFS.py
@@ -1,18 +1,18 @@
 
 # find Napoleon in family tree
 
-# we create a hashmap (dictionary) which will contain keys (nodes) and values (neighbors)
+# we create a hashmap (dictionary) which will contain keys (names as nodes) as values (parents as neighbors)
 
 from collections import deque
 family_tree = {}
 family_tree["me"] = ["Dostoyevsky", "Akhmatova"]
 family_tree["Dostoyevsky"] = ["Stalin", "Lenin"]
 family_tree["Akhmatova"] = ["Trotsky", "Brodsky"]
-family_tree["Stalin"] = ["Napoleonis", "Hitler"]
+family_tree["Stalin"] = ["Napoleon", "Hitler"]
 family_tree["Lenin"] = ["Nikolay First", "Yekaterina Second"]
 family_tree["Trotsky"] = []
 family_tree["Brodsky"] = []
-family_tree["Napoleonis"] = []
+family_tree["Napoleon"] = []
 family_tree["Hitler"] = []
 family_tree["Nikolay First"] = []
 family_tree["Yekaterina Second"] = []
@@ -22,20 +22,22 @@ def is_napoleon(name):
     return name == "Napoleon"
 
 
-def napoleon_search(key, graph):
+def napoleon_search(name, graph):
+    path = []
     checked = []
-    queue = deque()
-    queue += graph[key]
+    queue = deque([(name, path)])
 
     while queue:
-        person = queue.popleft()
+        person, path = queue.popleft()
+
         if person not in checked:
             if is_napoleon(person):
-                print("I am Napoleon's heir!")
-                return True
+                print(name, "is Napoleon's heir!")
+                return path + [person]
             else:
                 checked.append(person)
-                queue += graph[person]
+                for friend in graph[person]:
+                    queue.append((friend, path+[person]))
     return False
 
 
